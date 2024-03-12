6acfa7d6c51fbd1cedacb5811b3885c8c10706c0
Alexei Egorov <zibbidizibbidibom@gmail.com>
2024-03-12 10:30:09 +0300

    feat: add main service

--- main-service/gateway/src/main/java/ru/practicum/user/controller/AdminUserClientController.java
+++ main-service/gateway/src/main/java/ru/practicum/user/controller/AdminUserClientController.java
@@ -1,13 +1,15 @@
 package ru.practicum.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.UserDto;
 import ru.practicum.model.ConstraintViolationException;
 import ru.practicum.user.UserClient;
-import ru.practicum.dto.UserDto;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
@@ -20,10 +22,19 @@ import java.util.List;
 @RequestMapping(path = "/admin/users")
 public class AdminUserClientController {
     private final UserClient client;
+    private final ObjectMapper objectMapper;
 
     @PostMapping
-    public ResponseEntity<Object> add(@RequestBody @Valid UserDto userDto) {
-        return client.add(userDto);
+    public ResponseEntity<?> add(@RequestBody @Valid UserDto userDto) {
+        ResponseEntity<Object> responseEntity = client.add(userDto);
+
+        if (responseEntity.getStatusCode() == HttpStatus.CREATED) {
+            UserDto addedUserDto = objectMapper.convertValue(responseEntity.getBody(), UserDto.class);;
+            // Additional logic or validation if needed
+            return ResponseEntity.status(HttpStatus.CREATED).body(addedUserDto);
+        } else {
+            return ResponseEntity.status(responseEntity.getStatusCode()).body("An error occurred while adding the user");
+        }
     }
 
     @GetMapping
